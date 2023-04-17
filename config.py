1dde51537af6c7113896da7c08daaa88de954483
Your Name <you@example.com>
2023-04-17 23:50:35 +0800

    api2d support

--- config.py
+++ config.py
@@ -44,7 +44,7 @@ WEB_PORT = -1
 MAX_RETRY = 2
 
 # OpenAI模型选择是（gpt4现在只对申请成功的人开放）
-LLM_MODEL = "gpt-3.5-turbo" # 可选 "chatglm"
+LLM_MODEL = "api2d-gpt-3.5-turbo" # 可选 "chatglm"
 AVAIL_LLM_MODELS = ["gpt-3.5-turbo", "chatglm", "gpt-4", "api2d-gpt-4", "api2d-gpt-3.5-turbo"]
 
 # 本地LLM模型如ChatGLM的执行方式 CPU/GPU
